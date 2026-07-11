1c307f8fdd9ca8f6b829a60360f2fed03444dccd
egor <egorrazancev67@gmail.com>
2026-07-11 19:27:57 +0300

    modify readme

--- kernel/include/version.h
+++ kernel/include/version.h
@@ -1,21 +1,11 @@
-/*
- *  SPDX-License-Identifier: MIT
- *
- *  8086-OS/kernel/include/version.h
- *
- *  Copyright (C) 2026  sosiska565
- *
- *  May be freely distributed as part of 8086-OS.
- */
-
 #ifndef _GENERATED_VERSION_H
 #define _GENERATED_VERSION_H
 
 #define OS_NAME          "8086-OS"
 #define OS_VERSION_MAJOR 0
 #define OS_VERSION_MINOR 9
-#define OS_VERSION_PATCH 7
+#define OS_VERSION_PATCH 8
 #define OS_VERSION_EXTRA "beta"
-#define OS_RELEASE       "0.9.7-beta"
+#define OS_RELEASE       "0.9.8-beta"
 
 #endif
